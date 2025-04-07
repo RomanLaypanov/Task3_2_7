4b5f0e3a5adc0e300586d44100316dedc9fb0900
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-04-07 15:05:40 +0300

    Задача 3.2.7 третия проверка

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        boolean attempt = false;
+        RobotConnectionManager robotConnectionManager = new RobotConnectionManager() {
+            @Override
+            public RobotConnection getConnection() {
+                return new RobotConnection() {
+                    @Override
+                    public void moveRobotTo(int x, int y) {
+                        System.out.println("Робот перемещен в точку (" + x + ", " + y + ")");
+                    }
 
-        for (int i = 0; !attempt && (i < 3); ++i) {
-            try (Robot robot = new Robot(1, 2)) {
-                RobotConnection connection = robot.getConnection();
-
-                if (connection != null) {
-                    robot.moveRobotTo(5, 9);
-                    attempt = true;
-                } else {
-                    System.out.println("Соединение не удалось установить.");
-                }
-            } catch (RobotConnectionException ignored) {
+                    @Override
+                    public void close() {
+                        System.out.println("Соединение закрыто");
+                    }
+                };
             }
-        }
+        };
 
-        if (!attempt) {
-            throw new RobotConnectionException("Соединение не установленно");
-        }
+        Robot.moveRobot(robotConnectionManager, 2, 5);
     }
 }
--- src/Robot.java
+++ src/Robot.java
@@ -1,36 +1,18 @@
-public class Robot implements RobotConnection, RobotConnectionManager {
-    private int xR = 0;
-    private int yR = 0;
+public class Robot {
+    public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
+        RobotConnectionException lastException = null;
 
-    Robot(int xR, int yR) {
-        this.xR = xR;
-        this.yR = yR;
-    }
-
-    @Override
-    public void moveRobotTo(int x, int y) {
-        xR = x;
-        yR = y;
-        System.out.println("x = " + xR + ", " + "y = " + yR);
-    }
-
-    @Override
-    public void close() {
-        System.out.println("Соеденение закрыто");
-    }
-
-    @Override
-    public RobotConnection getConnection() {
-        return new RobotConnection() {
-            @Override
-            public void moveRobotTo(int x, int y) {
-
+        for (int attempt = 0; attempt < 3; attempt++) {
+            try (RobotConnection connection = robotConnectionManager.getConnection()) {
+                connection.moveRobotTo(toX, toY);
+                return;
+            } catch (RobotConnectionException e) {
+                lastException = e;
+            } catch (Exception e) {
+                throw e;
             }
+        }
 
-            @Override
-            public void close() {
-
-            }
-        };
+        throw new RobotConnectionException("Не удалось после 3 попыток", lastException);
     }
 }
