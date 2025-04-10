07e1155448be9e08d45712a1f169a884d4d1dd3d
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-04-10 20:01:32 +0300

    Задача 3.2.7 четвертая проверка

--- src/Robot.java
+++ src/Robot.java
@@ -1,18 +1,16 @@
 public class Robot {
-    public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
-        RobotConnectionException lastException = null;
-
+    public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) throws RobotConnectionException {
         for (int attempt = 0; attempt < 3; attempt++) {
             try (RobotConnection connection = robotConnectionManager.getConnection()) {
                 connection.moveRobotTo(toX, toY);
-                return;
+                break;
             } catch (RobotConnectionException e) {
-                lastException = e;
+                if (attempt == 2) {
+                    throw e;
+                }
             } catch (Exception e) {
                 throw e;
             }
         }
-
-        throw new RobotConnectionException("Не удалось после 3 попыток", lastException);
     }
 }
